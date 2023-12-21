3b19fe032cb8f90dd7b60fdc59a51f1d44e79f70
kumaryoya <kumaryoya05210521@gmail.com>
2023-12-21 13:48:11 +0900

    セットアップ

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -7,8 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and bbbbb save to reload.
+        <p className='btn text-5xl underline'>
+          Edit <code>src/App.tsx</code> and save qqqqq to reload.
         </p>
         <a
           className="App-link"
